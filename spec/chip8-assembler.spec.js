557426bb51826cd233ce9d4a7048ff758f795e73
Brandon Montgomery <bmonty@gmail.com>
2014-04-08 17:07:57 -0400

    add get2Bytes helper method; add a couple more tests

--- spec/chip8-assembler.spec.js
+++ spec/chip8-assembler.spec.js
@@ -1,6 +1,14 @@
   describe('Chip 8 Assembler', function(){
   var assembler;
 
+  var get2Bytes = function(startIndex) {
+    if (startIndex == undefined) {
+      startIndex = 0;
+    }
+
+    return (assembler.output[startIndex] << 8) | assembler.output[startIndex + 1];
+  };
+
   beforeEach(function(){
     assembler = new Chip8Assembler();
   });
@@ -16,47 +24,47 @@
   describe('instruction processing', function(){
     it('trims whitespace from instruction', function(){
       assembler.processInput('  CLS  ');
-      expect(assembler.output[0]).toBe(0x00E0);
+      expect(get2Bytes()).toBe(0x00E0);
     });
 
     it('trims tabs from instruction', function(){
       assembler.processInput('\tCLS\t');
-      expect(assembler.output[0]).toBe(0x00E0);
+      expect(get2Bytes()).toBe(0x00E0);
     });
 
     it('we\'re going to need comments', function() {
       assembler.processInput('CLS ; here is a comment');
-      expect(assembler.output[0]).toBe(0x00E0);
+      expect(get2Bytes()).toBe(0x00E0);
     });
 
     it('processes mulitple lines of instructions', function(){
       assembler.processInput('CLS\nCLS');
 
-      expect(assembler.output[0]).toBe(0x00E0);
-      expect(assembler.output[1]).toBe(0x00E0);
+      expect(get2Bytes()).toBe(0x00E0);
+      expect(get2Bytes(2)).toBe(0x00E0);
     });
 
     it('is OK with blank lines', function(){
       assembler.processInput('CLS\n\nCLS');
 
-      expect(assembler.output[0]).toBe(0x00E0);
-      expect(assembler.output[1]).toBe(0x00E0);
+      expect(get2Bytes()).toBe(0x00E0);
+      expect(get2Bytes(2)).toBe(0x00E0);
     });
 
     it('is OK with comment-only lines', function(){
       assembler.processInput('; here is a comment\nCLS');
 
-      expect(assembler.output[0]).toBe(0x00E0);
+      expect(get2Bytes()).toBe(0x00E0);
     });
 
     it('is not case sensitive', function(){
       assembler.processInput('cls');
-      expect(assembler.output[0]).toBe(0x00E0);
+      expect(get2Bytes()).toBe(0x00E0);
     });
 
     it('detects label', function(){
       assembler.processInput('MYLABEL: CLS ; clear screen');
-      expect(assembler.output[0]).toBe(0x00E0);
+      expect(get2Bytes()).toBe(0x00E0);
     });
 
     it('replaces label with memory address', function(){
@@ -65,7 +73,22 @@
         '         JP    MYLABEL'
       ];
       assembler.processInput(instructions.join('\n'));
-      expect(assembler.output[1]).toBe(0x1200);
+      expect(get2Bytes(2)).toBe(0x1200);
+    });
+
+    it('replaces label with memory address (more comlexity)', function(){
+      var instructions = [
+        '     CLS',
+        '     CLS',
+        '     CLS',
+        'LBL: CLS',
+        '     CLS',
+        '     CLS',
+        '     CLS',
+        '     JP   LBL'
+      ];
+      assembler.processInput(instructions.join('\n'));
+      expect(get2Bytes(14)).toBe(0x1206);
     });
 
     it('replaces label with memory address which isn\'t yet defined', function(){
@@ -74,208 +97,218 @@
         'MYLABEL: CLS'
       ];
       assembler.processInput(instructions.join('\n'));
-      expect(assembler.output[0]).toBe(0x1202);
+      expect(get2Bytes()).toBe(0x1202);
     });
   });
 
   describe('specific instructions', function(){
-    it('compiles CLS to 0x0E00', function(){
+    it('compiles CLS', function(){
       assembler.processInput('CLS');
-      expect(assembler.output[0]).toBe(0x00E0);
+      expect(get2Bytes()).toBe(0x00E0);
+    });
+
+    it('compiles RET', function(){
+      assembler.processInput('RET');
+      expect(get2Bytes()).toBe(0x00EE);
     });
 
     it('compiles SYS', function(){
       assembler.processInput('SYS #0400');
-      expect(assembler.output[0]).toBe(0x0400);
+      expect(get2Bytes()).toBe(0x0400);
     });
 
     it('compiles JP with address', function(){
       assembler.processInput('JP #0500');
-      expect(assembler.output[0]).toBe(0x1500);
+      expect(get2Bytes()).toBe(0x1500);
     });
 
     it('compiles JP with V0 and address', function(){
       assembler.processInput('JP V0, #0508');
-      expect(assembler.output[0]).toBe(0xB508);
+      expect(get2Bytes()).toBe(0xB508);
     });
 
     it('compiles CALL', function(){
       assembler.processInput('CALL #0220');
-      expect(assembler.output[0]).toBe(0x2220);
+      expect(get2Bytes()).toBe(0x2220);
     });
 
     it('compiles SE with V register and value', function(){
       assembler.processInput('SE V0, #A0');
-      expect(assembler.output[0]).toBe(0x30A0);
+      expect(get2Bytes()).toBe(0x30A0);
     });
 
     it('compiles SE with 2 V registers', function(){
       assembler.processInput('SE V0, VE');
-      expect(assembler.output[0]).toBe(0x50E0);
+      expect(get2Bytes()).toBe(0x50E0);
     });
 
     it('compiles SNE with V register and value', function(){
       assembler.processInput('SNE VA, #AA');
-      expect(assembler.output[0]).toBe(0x4AAA);
+      expect(get2Bytes()).toBe(0x4AAA);
     });
 
     it('compiles SNE with 2 V registers', function(){
       assembler.processInput('SNE VA, VB');
-      expect(assembler.output[0]).toBe(0x9AB0);
+      expect(get2Bytes()).toBe(0x9AB0);
     });
 
     it('compiles LD with V register and hex value', function(){
       assembler.processInput('LD V9, #42');
-      expect(assembler.output[0]).toBe(0x6942);
+      expect(get2Bytes()).toBe(0x6942);
     });
 
     it('compiles LD with V register and dec value', function(){
       assembler.processInput('LD V9, 42');
-      expect(assembler.output[0]).toBe(0x6900 + 42);
+      expect(get2Bytes()).toBe(0x6900 + 42);
     });
 
     it('compiles LD with 2 V registers', function(){
       assembler.processInput('LD V9, V2');
-      expect(assembler.output[0]).toBe(0x8920);
+      expect(get2Bytes()).toBe(0x8920);
     });
 
     it('compiles LD with I and address', function(){
       assembler.processInput('LD I, #51A');
-      expect(assembler.output[0]).toBe(0xA51A);
+      expect(get2Bytes()).toBe(0xA51A);
     });
 
     it('compiles LD with V register and DT', function(){
       assembler.processInput('LD V2, DT');
-      expect(assembler.output[0]).toBe(0xF207);
+      expect(get2Bytes()).toBe(0xF207);
     });
 
     it('compiles LD with V register and K', function(){
       assembler.processInput('LD V2, K');
-      expect(assembler.output[0]).toBe(0xF20A);
+      expect(get2Bytes()).toBe(0xF20A);
     });
 
     it('compiles LD with DT and V register', function(){
       assembler.processInput('LD DT, V8');
-      expect(assembler.output[0]).toBe(0xF815);
+      expect(get2Bytes()).toBe(0xF815);
     });
 
     it('compiles LD with ST and V register', function(){
       assembler.processInput('LD ST, V7');
-      expect(assembler.output[0]).toBe(0xF718);
+      expect(get2Bytes()).toBe(0xF718);
     });
 
     it('compiles LD with F and V register', function(){
       assembler.processInput('LD F, V6');
-      expect(assembler.output[0]).toBe(0xF629);
+      expect(get2Bytes()).toBe(0xF629);
     });
 
     it('compiles LD with B and V register', function(){
       assembler.processInput('LD B, V6');
-      expect(assembler.output[0]).toBe(0xF633);
+      expect(get2Bytes()).toBe(0xF633);
     });
 
     it('compiles LD with [I] and V register', function(){
       assembler.processInput('LD [I], V5');
-      expect(assembler.output[0]).toBe(0xF555);
+      expect(get2Bytes()).toBe(0xF555);
     });
 
     it('compiles LD with V register and [I]', function(){
       assembler.processInput('LD V2, [I]');
-      expect(assembler.output[0]).toBe(0xF265);
+      expect(get2Bytes()).toBe(0xF265);
     });
 
     it('compiles ADD with V register and value', function(){
       assembler.processInput('ADD V1, #09');
-      expect(assembler.output[0]).toBe(0x7109);
+      expect(get2Bytes()).toBe(0x7109);
     });
 
     it('compiles ADD with 2 V registers', function(){
       assembler.processInput('ADD V7, V8');
-      expect(assembler.output[0]).toBe(0x8784);
+      expect(get2Bytes()).toBe(0x8784);
     });
 
     it('compiles ADD with I and V register', function(){
       assembler.processInput('ADD I, V5');
-      expect(assembler.output[0]).toBe(0xF51E);
+      expect(get2Bytes()).toBe(0xF51E);
     });
 
     it('compiles OR', function(){
       assembler.processInput('OR V1, V2');
-      expect(assembler.output[0]).toBe(0x8121);
+      expect(get2Bytes()).toBe(0x8121);
     });
 
     it('compiles AND', function(){
       assembler.processInput('AND V2, V3');
-      expect(assembler.output[0]).toBe(0x8232);
+      expect(get2Bytes()).toBe(0x8232);
     });
 
     it('compiles XOR', function(){
       assembler.processInput('XOR V4, VF');
-      expect(assembler.output[0]).toBe(0x84F3);
+      expect(get2Bytes()).toBe(0x84F3);
     });
 
     it('compiles SUB', function(){
       assembler.processInput('SUB V4, V8');
-      expect(assembler.output[0]).toBe(0x8485);
+      expect(get2Bytes()).toBe(0x8485);
     });
 
     it('compiles SHR for 2 registers', function(){
       assembler.processInput('SHR V4, VB');
-      expect(assembler.output[0]).toBe(0x84B6);
+      expect(get2Bytes()).toBe(0x84B6);
     });
 
     it('compiles SHR for 1 register', function(){
       assembler.processInput('SHR VC');
-      expect(assembler.output[0]).toBe(0x8CC6);
+      expect(get2Bytes()).toBe(0x8CC6);
     });
 
     it('compiles SUBN', function(){
       assembler.processInput('SUBN VC, VD');
-      expect(assembler.output[0]).toBe(0x8CD7);
+      expect(get2Bytes()).toBe(0x8CD7);
     });
 
     it('compiles SHL for 2 registers', function(){
       assembler.processInput('SHL V4, VB');
-      expect(assembler.output[0]).toBe(0x84BE);
+      expect(get2Bytes()).toBe(0x84BE);
     });
 
     it('compiles SHL for 1 register', function(){
       assembler.processInput('SHL VC');
-      expect(assembler.output[0]).toBe(0x8CCE);
+      expect(get2Bytes()).toBe(0x8CCE);
     });
 
     it('compiles RND', function(){
       assembler.processInput('RND VD, #0F');
-      expect(assembler.output[0]).toBe(0xCD0F);
+      expect(get2Bytes()).toBe(0xCD0F);
     });
 
     it('compiles DRW', function(){
       assembler.processInput('DRW V0, V1, #A');
-      expect(assembler.output[0]).toBe(0xD01A);
+      expect(get2Bytes()).toBe(0xD01A);
     });
 
     it('compiles SKP', function(){
       assembler.processInput('SKP VE');
-      expect(assembler.output[0]).toBe(0xEE9E);
+      expect(get2Bytes()).toBe(0xEE9E);
     });
 
     it('compiles SKNP', function(){
       assembler.processInput('SKNP V3');
-      expect(assembler.output[0]).toBe(0xE3A1);
+      expect(get2Bytes()).toBe(0xE3A1);
     });
 
     it('compiles DW', function(){
       assembler.processInput('DW #ABC2');
-      expect(assembler.output[0]).toBe(0xABC2);
+      expect(get2Bytes()).toBe(0xABC2);
     });
 
-    // it('compiles DB', function(){
-    //   assembler.processInput('DB #FF\nDB #EA');
-    //   expect(assembler.output[0]).toBe(0xFFEA);
-    // });
+    it('compiles DB - once', function(){
+      assembler.processInput('DB #FC');
+      expect(assembler.output[0]).toBe(0xFC);
+    });
+
+    it('compiles DB - twice', function(){
+      assembler.processInput('DB #FF\nDB #EA');
+      expect(get2Bytes()).toBe(0xFFEA);
+    });
   });
 
-  it('compiles example program', function(){
+  it('compiles example program from chasm manual', function(){
     var instructions = [
       'START: CLS',
       '       RND     V0,   #FF',
@@ -295,30 +328,88 @@
       '       LD      V0,   K',
       '       JP      START',
       '',
-      '       DW      #FFEA',
+      '       DB      #FF',
+      '       DB      #EA',
+      '',
       '       DW      #21AC'
     ];
 
     var expectedOutput = [
-      0x00E0,
-      0xC0FF,
-      0xA224,
-      0xF033,
-      0xF265,
-      0xF029,
-      0x6000,
-      0x6300,
-      0xD035,
-      0xF129,
-      0x6005,
-      0xD035,
-      0xF229,
-      0x600A,
-      0xD035,
-      0xF00A,
-      0x1200,
-      0xFFEA,
-      0x21AC
+      0x00, 0xE0,
+      0xC0, 0xFF,
+      0xA2, 0x24,
+      0xF0, 0x33,
+      0xF2, 0x65,
+      0xF0, 0x29,
+      0x60, 0x00,
+      0x63, 0x00,
+      0xD0, 0x35,
+      0xF1, 0x29,
+      0x60, 0x05,
+      0xD0, 0x35,
+      0xF2, 0x29,
+      0x60, 0x0A,
+      0xD0, 0x35,
+      0xF0, 0x0A,
+      0x12, 0x00,
+      0xFF, 0xEA,
+      0x21, 0xAC
+    ];
+    assembler.processInput(instructions.join('\n'));
+    expect(assembler.output).toEqual(expectedOutput);
+  });
+
+  it('compiles example program for displaying characters', function(){
+    var instructions = [
+      '           LD   V0,      0     ; set V0 (the character to display)',
+      '           LD   V1,      1     ; set x (V1) to 1 (the x coord of sprite)',
+      '           LD   V2,      1     ; set y (V2) to 1 (the y coord of sprite)',
+      '           LD   V4,      57    ; set x_max (V4) to 57 - this is the maximum x value at which a sprite will be displayed',
+
+      'CHECKEXIT: SNE  V0,      #10   ; if V0 is #10, jump to end; otherwise skip next instruction',
+      '           JP   #FFF           ; exit',
+      '           CALL DISPCHAR       ; call subroutine to display character',
+      '           JP   CHECKEXIT      ; when subroutine returns, go back to if statement to check to see if program should exit',
+
+      'DISPCHAR:  LD   F,       V0    ; set I to the start address of the character in V0',
+      '           DRW  V1,      V2, 5 ; draw sprite 5 bytes in size at coords x, y (V1, V2)',
+      '           ADD  V0,      1     ; increment char to display (V0) by 1',
+      '           ADD  V1,      5     ; increment x (V1) by 5',
+
+      '; if x > x_max, increment y by 6 and set x to 0',
+      '           LD,  V3,      V1    ; copy value from x (V1) to V3',
+      '           SUB, V3,      V4    ; subtract x_max (V4) from V3',
+      '           SE   VF,      0     ; skip next instruction if result was positive (VF = 0)',
+      '           JP   NEG            ; jump to negative case',
+      '           JP   RET            ; jump to return from subroutine',
+
+      '  NEG:     LD   V1,      1     ; set x (V1) to 1',
+      '           ADD  V2,      6     ; increment y (V2) by 6',
+
+      '  RET:     RET                 ; return from subroutine'
+    ];
+
+    var expectedOutput = [
+        0x60, 0x00,
+        0x61, 0x01,
+        0x62, 0x01,
+        0x64, 0x39,
+        0x40, 0x10,
+        0x1F, 0xFF,
+        0x22, 0x10,
+        0x12, 0x08,
+        0xF0, 0x29,
+        0xD1, 0x25,
+        0x70, 0x01,
+        0x71, 0x05,
+        0x83, 0x10,
+        0x83, 0x45,
+        0x3F, 0x00,
+        0x12, 0x22,
+        0x12, 0x26,
+        0x61, 0x01,
+        0x72, 0x06,
+        0x00, 0xEE 
     ];
     assembler.processInput(instructions.join('\n'));
     expect(assembler.output).toEqual(expectedOutput);
